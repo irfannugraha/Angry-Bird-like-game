268dc3d32b5be029ee2b84a30c704ef2d26e2d65
irfannugraha <irfannugraha844@gmail.com>
2020-03-27 23:27:57 +0700

    First Commit

---
+++ Assets/Script/Black.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Black : Bird
+{
+    [SerializeField]
+    public GameObject blast;
+    public Renderer render;
+
+    public void OnCollisionEnter2D(Collision2D other) {
+        Collider.radius = 1f;
+        Instantiate(blast, transform.position, Quaternion.identity);
+        Destroy(this);
+        render.enabled = false;
+    }
+}
---
+++ Assets/Script/Explotion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explotion : MonoBehaviour
+{
+    public GameObject blast;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("start");
+        GameObject belast = (GameObject)Instantiate(blast);
+        belast.transform.position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
